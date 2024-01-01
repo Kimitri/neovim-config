32120c22960dd59eb1fe52e4e3aa7db53f5b4682
Kimmo Tapala <kimitri@gmail.com>
2024-01-01 15:22:01 +0200

    Fixing things for NixOS

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,7 @@ return {
         null_ls.builtins.completion.spell,
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.diagnostics.phpstan,
-        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.lua_format,
       },
     })
   end,
