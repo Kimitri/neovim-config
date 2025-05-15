e684546aedb48e2736d0d26410e3339e5a5b934a
Kimmo Tapala <kimitri@gmail.com>
2025-05-15 09:51:38 +0300

    Update Neovim and plugins

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,77 @@
 return {
   "williamboman/mason-lspconfig.nvim",
   config = function()
+    vim.lsp.config('mason-lspconfig', {
+      on_attach = function(client, bufnr)
+        require("lsp_signature").on_attach({
+          bind = true,
+          handler_opts = {
+            border = "rounded"
+          }
+        }, bufnr)
+      end
+    })
+
+    vim.lsp.config('ansiblels', {
+      settings = {
+        ansible = {
+          validation = {
+            enabled = true,
+            lint = {
+              enabled = false
+            }
+          }
+        }
+      }
+    })
+
+    vim.lsp.config('arduino_language_server', {
+      cmd = { "arduino-language-server" },
+      filetypes = { "arduino" },
+      root_dir = require("lspconfig.util").root_pattern("*.ino")
+    })
+
+    vim.lsp.config('phpactor', {
+      settings = {
+        phpactor = {
+          enable = true,
+          completion = {
+            enable = true
+          },
+          diagnostics = {
+            enable = false
+          },
+          goto = {
+            implementation = "goto_definition",
+            typeDefinition = "goto_type_definition"
+          },
+          hover = {
+            enable = true
+          },
+          index = {
+            references = true
+          },
+          rename = {
+            enable = true
+          },
+          signatureHelp = {
+            enable = true
+          },
+          workspaceSymbolProvider = true
+        }
+      }
+    })
+
+    vim.lsp.config('rust_analyzer', {
+      settings = {
+        ['rust_analyzer'] = {
+          diagnostics = {
+            enable = true;
+          }
+        }
+      }
+    })
+
     require("mason-lspconfig").setup {
       ensure_installed = {
         "ansiblels",
@@ -17,76 +88,6 @@ return {
         "vimls",
         "yamlls"
       }
-    }
-
-    require("mason-lspconfig").setup_handlers {
-      function (server_name)
-        require("lspconfig")[server_name].setup {}
-      end,
-      ["ansiblels"] = function ()
-        require("lspconfig").ansiblels.setup {
-          settings = {
-            ansible = {
-              validation = {
-                enabled = true,
-                lint = {
-                  enabled = false
-                }
-              }
-            }
-          }
-        }
-      end,
-      ["arduino_language_server"] = function ()
-        require("lspconfig").arduino_language_server.setup {
-          cmd = { "arduino-language-server" },
-          filetypes = { "arduino" },
-          root_dir = require("lspconfig.util").root_pattern("*.ino")
-        }
-      end,
-      ["phpactor"] = function ()
-        require("lspconfig").phpactor.setup {
-          settings = {
-            phpactor = {
-              enable = true,
-              completion = {
-                enable = true
-              },
-              diagnostics = {
-                enable = false
-              },
-              goto = {
-                implementation = "goto_definition",
-                typeDefinition = "goto_type_definition"
-              },
-              hover = {
-                enable = true
-              },
-              index = {
-                references = true
-              },
-              rename = {
-                enable = true
-              },
-              signatureHelp = {
-                enable = true
-              },
-              workspaceSymbolProvider = true
-            }
-          }
-        }
-      end,
-      ["rust_analyzer"] = function ()
-        require'lspconfig'.rust_analyzer.setup{
-          settings = {
-            ['rust_analyzer'] = {
-              diagnostics = {
-                enable = true;
-              }
-            }
-          }
-        }
-      end
     }
   end
 }
